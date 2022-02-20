2778f850f7d1d3e47a24a039e1847d4ec1d2dd26
Vancouver13 <97991554+Vancouver13@users.noreply.github.com>
2022-02-19 18:21:49 -0800

    add mock server (#9)

--- .mocharc.js
+++ .mocharc.js
@@ -1,6 +1,7 @@
 module.exports = {
     require: '@babel/register',
     spec: 'specs/**/*.spec.js',
-    ignore: 'spec/example.spec.js',
+    ignore: 'specs/example.spec.js',
     file: 'config/setup.js',
+    timeout: 15000
 }
